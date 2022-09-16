76eee25a184bc7c0601ce36b9ba5b2b7160165fc
jmternes <68615887+jmternes@users.noreply.github.com>
2022-09-16 12:51:40 -0500

    Add files via upload

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Avademy.\nStudent Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            byte pageNumber = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"True\" or \"False\"");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            byte hoursStudied = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}
