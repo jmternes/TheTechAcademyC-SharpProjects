dedd1beba585d23dedac9f12af752a28fa0016b8
jmternes <68615887+jmternes@users.noreply.github.com>
2022-09-23 13:04:53 -0500

    Add files via upload

---
+++ WhileAssignment/WhileAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WhileAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while loop
+            int i = 1;
+            while (i <= 3)
+            {
+                Console.WriteLine("This will be displayed 3 times.");
+                i++;
+            }
+            // do while loop
+            int x = 2;
+            int y = 3;
+            do
+            {
+                int sum = x + y;
+                Console.WriteLine(sum);
+                x++;
+            }
+            while (x <= 5);
+        }
+    }
+}
