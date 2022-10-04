9f0195e460d21772865e7e419567e8c29607f107
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-04 15:04:14 -0500

    Add files via upload

---
+++ ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>();
+            numList.Add(10);
+            numList.Add(20);
+            numList.Add(40);
+            numList.Add(80);
+
+            Console.WriteLine("Hello user. Please select a number.");
+            
+
+            try
+            {
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                foreach (int value in numList)
+                {
+                    int quotient = value / userNum;
+                    Console.WriteLine(value + " divided by your number (" + userNum + ") = " + quotient);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You can't divide by zero!");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
