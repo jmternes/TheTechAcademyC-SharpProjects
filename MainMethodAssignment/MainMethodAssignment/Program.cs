9f64d4c038a62ebda92c9668e6e8d70ba740b59c
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-06 16:07:52 -0500

    Add files via upload

---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass classy = new myClass();
+            Console.WriteLine(classy.addNumbers(3, 6));
+            Console.WriteLine(classy.subtractNumbers(19.5m, 18.5m));
+            Console.WriteLine(classy.stringToInt("8"));
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/myClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodAssignment
+{
+    public class myClass
+    {
+        public int addNumbers(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public int subtractNumbers(decimal c, decimal d)
+        {
+            int difference = Convert.ToInt32(c - d);
+            return difference;
+        }
+
+        public int stringToInt(string myString)
+        {
+            int converted = Convert.ToInt32(myString);
+            return converted;
+        }
+    }
+}
