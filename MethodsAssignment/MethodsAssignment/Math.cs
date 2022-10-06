8fd9c646d546f8e0ef7128f2cffc2fdc49122354
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-06 14:56:28 -0500

    Add files via upload

---
+++ MethodsAssignment/MethodsAssignment/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodsAssignment
+{
+    public class Math
+    {
+        public int plusOne(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        public int divideByTwo(int a, int c)
+        {
+            int quotient = a / c;
+            return quotient;
+        }
+
+        public int doubleIt(int a, int d)
+        {
+            int doubled = a * d;
+            return doubled;
+        }
+    }
+        
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("What number would you like to see actions performed on?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(math.plusOne(a, 1));
+            int c = 2;
+            Console.WriteLine(math.divideByTwo(a, c));
+            int d = 2;
+            Console.WriteLine(math.doubleIt(a, d));
+        }
+    }
+}
