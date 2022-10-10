13a59bae3f4aa09c1de2813e2c2b92c2ea669247
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-10 13:50:17 -0500

    Add files via upload

---
+++ MethodAssignment2/MethodAssignment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class
+            myClass myClass = new myClass();
+
+            // ask the user for number(s), take user's input to set the value of int a
+            Console.WriteLine("Please input 2 numbers, one at a time. (Or just 1 number)");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            // a try block for if the user enters a second value for b
+            try
+            {
+                int b = Convert.ToInt32(Console.ReadLine());
+                int result = myClass.mathFun(a, b);
+                Console.WriteLine("Result: " + result);
+            }
+            
+            // if no second number is given, we run the function with the optional parameter, which was set to 1 in myClass
+
+            catch
+            {
+                int result = myClass.mathFun(a);
+                Console.WriteLine("Result: " + result);
+            }
+            
+        }
+    }
+}
---
+++ MethodAssignment2/MethodAssignment2/myClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAssignment2
+{
+    public class myClass
+    {
+        public int mathFun(int a, int b = 1)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
