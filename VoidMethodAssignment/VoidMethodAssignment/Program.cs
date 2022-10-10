99320c82563010f0919729d88335a27e2a8793ad
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-10 14:06:49 -0500

    Add files via upload

---
+++ VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class instantiation
+            myClass myClass = new myClass();
+
+            // calling the method, passing 2 numbers
+            myClass.subtract(10, 5);
+
+            // calling the method, parameters by name
+            int a = 20;
+            int b = 10;
+            myClass.subtract(a, b);
+        }
+    }
+}
---
+++ VoidMethodAssignment/VoidMethodAssignment/myClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethodAssignment
+{
+    class myClass
+    {
+        public void subtract(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine(result);
+        }
+    }
+}
