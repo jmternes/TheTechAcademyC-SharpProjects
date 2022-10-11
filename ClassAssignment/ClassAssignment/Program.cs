b744308bd1352234966076d9f8c99fc3330bde77
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-11 12:16:49 -0500

    Add files via upload

---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiation nation
+            classy classy = new classy();
+
+            // user input set to int a, then calling the method with that number. will display half of int a
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            classy.halfIt(a);
+
+            // method without parameters, just serves to write a sentence on the console.
+            classy.noParameters();
+
+            // out parameter method. declare variable c without giving value, then pass the variable with out keyword
+            int b;
+            classy.outParameter(out b);
+            Console.WriteLine("Addition of the value: {0}", b);
+
+            // method overloaded
+            classy.noParameters(10);
+
+            
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/classy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAssignment
+{
+    class classy
+    {
+        public void halfIt(int a)
+        {
+            int result = a / 2;
+            Console.WriteLine(result);
+        }
+
+        // method with no parameters
+        public void noParameters()
+        {
+            Console.WriteLine("2 + 2 = " + 4);
+        }
+
+        // method with out parameter, += operand to add and assign the result to b
+        public void outParameter(out int b)
+        {
+            b = 20;
+            b += 2;
+        }
+
+       // method overloading, by changing number of parameters
+        public void noParameters(int i = 0)
+        {
+            Console.WriteLine("2 + 2 = " + 4 + " and here's our argument: " + i);
+        }
+    }
+
+    // make a static class, empty but this is how!
+
+    static class Static_Class
+    {
+
+    }
+}
