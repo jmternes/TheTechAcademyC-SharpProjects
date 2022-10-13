311ae58c9aa4ac10f1eeb4308c6fd279d317a105
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-13 12:36:57 -0500

    Add files via upload

---
+++ AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassAssignment
+{
+    // public abstract (or base) class created called Person
+    public abstract class Person
+    {
+        // created two properties, first and last name
+        public string firstName = "John";
+        public string lastName = "Doe";
+
+        // method say name created
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
