bf364c844a8249ab53b182db970a37c8a22f6bbe
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-17 16:24:22 -0500

    Add files via upload

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadingOperators
+{
+    // class called Employee created
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+    public static bool operator ==(Employee e1, Employee e2)
+    {
+        if (e1.Id == e2.Id)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static bool operator !=(Employee e1, Employee e2)
+    {
+        if (e1.Id != e2.Id)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+            Console.WriteLine(e1 == e2);
+        }
+        
+    }
+}
