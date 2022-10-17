95ac5fe673839146bc97f64e8d372ba38ee760f3
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-17 16:54:44 -0500

    Add files via upload

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadingOperators
+{
+    // class called Employee created
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        // overloading the == operator, with employee objects e1 and e2
+        public static bool operator ==(Employee e1, Employee e2)
+        {
+            if (e1.Id == e2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        // Comparison operators are overloaded in pairs, so the same overloading method for the opposite !=
+        public static bool operator !=(Employee e1, Employee e2)
+        {
+            if (e1.Id != e2.Id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+    }
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+            e1.Id = 1;
+            e1.firstName = "Spongebob";
+            e1.lastName = "Squarepants";
+            e2.Id = 2;
+            e2.firstName = "Squidward";
+            e2.lastName = "Tentacles";
+            Console.WriteLine(e1 == e2);
+            Console.ReadLine();
+        }
+        
+    }
+}
