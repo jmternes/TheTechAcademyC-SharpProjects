53639df89f57c74ecfc1d655b435d9bde8de183c
jmternes <68615887+jmternes@users.noreply.github.com>
2022-10-18 13:59:47 -0500

    Add files via upload

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        public enum days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day");
+            try
+            {
+                // Enum.Parse method will return the enum type
+                days currentDay = (days)Enum.Parse(typeof(days),Console.ReadLine());
+                Console.WriteLine("Today is: " + currentDay);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            
+        }
+    }
+}
