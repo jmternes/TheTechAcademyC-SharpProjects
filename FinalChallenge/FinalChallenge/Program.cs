c6caa4b74f53e093317c062cd33ad272be00b232
jmternes <68615887+jmternes@users.noreply.github.com>
2022-11-11 12:52:59 -0600

    Add files via upload

---
+++ FinalChallenge/FinalChallenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student()
+                {
+                    StudentId = 0,
+                    StudentName = "Jane Doe",
+                    DateOfBirth = new DateTime(1993, 4, 22),
+                    Height = 53,
+                    Weight = 152
+                };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
